f0c99ef9fc50e6895b4e5af65156e9b00dc363a9
RAZAFIMANDIMBY Zo Hery Niaina Jacky <zorazafi@CPPSYSTEM.LOCAL>
2026-04-21 22:48:52 +0300

    updload file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const allowedOrigins = ["http://localhost:5173", "https://frontafripoksv2.vercel.app"];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
